6dc5177b3acccd37c115bb981e07554b6033b9c6
wacoo <wabaham9@gmail.com>
2024-06-26 11:00:10 +0300

    after report deployment success

--- main/settings.py
+++ main/settings.py
@@ -82,27 +82,27 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mssql',
+        'NAME': 'vehicle_dispatch',
+        'USER': 'sa',
+        'PASSWORD': '',
+        'HOST': 'localhost\\SQL2019',
+        'OPTIONS': {
+            'driver': 'ODBC Driver 18 for SQL Server',
+            'extra_params': 'TrustServerCertificate=yes;Encrypt=no',
+            'unicode_results': True,
+        },
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'mssql',
-#         'NAME': 'vehicle_dispatch',
-#         'USER': 'sa',
-#         'PASSWORD': '',
-#         'HOST': 'localhost\\SQL2019',
-#         'OPTIONS': {
-#             'driver': 'ODBC Driver 18 for SQL Server',
-#             'extra_params': 'TrustServerCertificate=yes;Encrypt=no',
-#             'unicode_results': True,
-#         },
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
